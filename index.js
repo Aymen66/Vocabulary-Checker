0823d9fb5479e1415048cdf5ba572b113d584093
Aymen <111906131+Aymen66@users.noreply.github.com>
2022-11-28 22:59:12 +0000

    changes

--- index.js
+++ index.js
@@ -106,11 +106,25 @@
       }deleteBtn();
 
   //  I used the Blob to excute the download button 
-      function download(text, name, type) {   
-        var a = document.createElement("a");
-        var file = new Blob([wordsList], { type: type });
-        a.href = URL.createObjectURL(file);
-        a.download = name;  
-        a.click();
-     }
+  function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(wordsList));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+  }
+  
+  // Start file download.
+  document.getElementById("dwn-btn").addEventListener("click", function(){
+  // Start the download of yournewfile.txt file with the content from the text area
+      var text = document.getElementById("text-val").value;
+      var filename = "yournewfile.txt";
+      
+      download(filename, wordsList);
+  }, false);
     
