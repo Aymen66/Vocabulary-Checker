eac296afc56e6ddcbb4d9d180861bb45a5bdfecf
Aymen <111906131+Aymen66@users.noreply.github.com>
2022-11-28 21:11:13 +0000

    changes

--- index.js
+++ index.js
@@ -22,13 +22,13 @@
   
     if (typeof window !== "undefined") {
       console.log("In Browser");
-      let leadsFromLocalStorage = JSON.parse( localStorage.getItem("wordsList") )
-      console.log(leadsFromLocalStorage)
+      let wordsFromLocalStorage = JSON.parse( localStorage.getItem("wordsList") )
+      console.log(wordsFromLocalStorage)
       // 1. Check if leadsFromLocalStorage is truthy
   // 2. If so, set myLeads to its value and call renderLeads()
   
-  if (leadsFromLocalStorage) {
-    wordsList = leadsFromLocalStorage
+  if (wordsFromLocalStorage) {
+    wordsList = wordsFromLocalStorage
   }
     }
     else {
