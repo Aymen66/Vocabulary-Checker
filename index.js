2a536ecbbb24c608ab2091578c14bd03db5a7b32
Aymen <111906131+Aymen66@users.noreply.github.com>
2022-11-29 00:13:20 +0000

    changes

--- index.js
+++ index.js
@@ -115,10 +115,25 @@
     //     a.click();
     //  }
     
-    function download(wordsList, type) {
-      var blob = new Blob([wordsList],{type:type});
-      var link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      link.download = "fileName";
-      link.click();
+    
+    function download(filename, text) {
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(wordsList));
+      element.setAttribute('download', filename);
+    
+      element.style.display = 'none';
+      document.body.appendChild(element);
+    
+      element.click();
+    
+      document.body.removeChild(element);
     }
+    
+    // Start file download.
+    document.getElementById("dwn-btn").addEventListener("click", function(){
+    // Start the download of yournewfile.txt file with the content from the text area
+        var text = document.getElementById("text-val").value;
+        var filename = "yournewfile.txt";
+        
+        download(filename, wordsList);
+    }, false);
