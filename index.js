003ed29655875606bd452a0c08c142621dbfea4b
Aymen <111906131+Aymen66@users.noreply.github.com>
2022-11-30 19:46:49 +0000

    changes

--- index.js
+++ index.js
@@ -132,7 +132,7 @@
     // Start file download.
     document.getElementById("dwn-btn").addEventListener("click", function(){
     // Start the download of yournewfile.txt file with the content from the text area
-        var text = document.getElementById("text").value;
+        var text = wordsList;
         var filename = "yournewfile.txt";
         
         download(filename, wordsList);
